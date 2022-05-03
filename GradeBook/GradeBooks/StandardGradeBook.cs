41730ab0402b914c291e0c7d662c5fc2737be264
Graham Steele <graham.steele@me.com>
2022-05-03 14:09:24 +0100

    Created new gradebook types

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
@@ -10,7 +6,7 @@ namespace GradeBook.GradeBooks
     {
         public StandardGradeBook(string name) : base(name)
         {
-
+            Type = GradeBookType.Standard;
         }
 
     }
